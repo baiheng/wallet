f623a29a9a5b465788bf9e39528362c6113f17cb
old kidd <850477916@qq.com>
2018-04-14 23:49:43 +0800

    添加反向代理功能
    1. 在 access/proxyConfig 中配置代理信息

--- access/app.js
+++ access/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var enhanceProxyPass = require('./middlewares/proxyPass');
 
 var app = express();
 
@@ -21,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+enhanceProxyPass(app);
+// app.use(proxies(app));
 
 app.use('/', index);
 app.use('/users', users);
