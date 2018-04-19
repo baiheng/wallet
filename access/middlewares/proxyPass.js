51196ec6ec68fb8e6001b7618d49d63dba8f1c7a
old kidd <850477916@qq.com>
2018-04-19 23:21:25 +0800

    修复版本控制bug

--- access/middlewares/proxyPass.js
+++ access/middlewares/proxyPass.js
@@ -5,7 +5,15 @@ const proxyConfig = require('../proxyConfig');
 
 const enhanceProxyPass = (app) => {
 	const config = proxyConfig || {};
-	_.forEach(config, (dest, path) => app.use(path, proxy({ target: dest, changeOrigin: true })))
+	_.forEach(config, (rules, version) => {
+		_.forEach(rules, (dest, path) => app.use(`/${version}${path}`, proxy({
+			target: dest,
+			changeOrigin: true,
+			pathRewrite: {
+        [`/${version}${path}`] : path.replace(/\*$/, ''),     // rewrite path
+      },
+		})))
+	})
 }
 
 module.exports = enhanceProxyPass;
