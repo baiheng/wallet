654e00a57e8b708b5bb1b07080c5d677cc5295e3
old kidd <850477916@qq.com>
2018-05-02 23:36:13 +0800

    eth 分页功能

--- eth/routes/personalTransaction.js
+++ eth/routes/personalTransaction.js
@@ -1,18 +1,24 @@
 const etherscan = require('../libs/etherscan');
 const { responseError, responseSuccess } = require('../libs/response');
 
+const PAGESIZE = 10;
 const personalTransaction = (req, res, next) => {
-	const { address } = req.query;
+	const { address, page } = req.query;
 	if (!address) {
 		return responseError(res, 50001, 'address should not be empty');
 	}
+	let pageNum = !isNaN(parseInt(page)) && parseInt(page) >= 0 ? parseInt(page) : 0;
+
+	// https://api.etherscan.io/api?module=account&action=txlist&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=YourApiKeyToken
 	const option = {
 		address,
 		module: 'account',
 		action: 'txlist',
 		startblock: 0,
 		endblock: 99999999,
-		sort: 'asc',
+		sort: 'desc',
+		page: pageNum + 1,
+		offset: PAGESIZE,
 	}
 	etherscan(option)
 		.then(data => {
