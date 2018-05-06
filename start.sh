2c7070e13ec47fd840b42790f19e1e571122945c
old kidd <850477916@qq.com>
2018-05-06 22:03:09 +0800

    update

--- start.sh
+++ start.sh
@@ -50,10 +50,10 @@ createLogFile(){
 
 startGeth(){
 	if [[ "$debug" = true ]]; then
-		nohup geth --light --lightkdf --rpc --rpccorsdomain "localhost" --rpcapi "db,eth,net,web3" --rpcport "17080" --rinkeby  >> "$logPath/$targetProcess.log" 2>&1 &
+		nohup geth --lightserv 50 --lightkdf --rpc --rpccorsdomain "localhost" --rpcapi "db,eth,net,web3" --rpcport "17080" --rinkeby  >> "$logPath/$targetProcess.log" 2>&1 &
 		return;
 	else
-		nohup geth --light --lightkdf --rpc --rpccorsdomain "localhost" --rpcapi "db,eth,net,web3" --rpcport "12080" >> "$logPath/$targetProcess.log" 2>&1 &
+		nohup geth --lightserv 50 --lightkdf --rpc --rpccorsdomain "localhost" --rpcapi "db,eth,net,web3" --rpcport "12080" >> "$logPath/$targetProcess.log" 2>&1 &
 	fi
 }
 
