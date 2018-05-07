7b69d7b727458426e09cf8569748bc2703f3d76e
old kidd <850477916@qq.com>
2018-05-07 22:41:15 +0800

    修改eth balance单位，添加nonce返回

--- eth/routes/balance.js
+++ eth/routes/balance.js
@@ -4,6 +4,7 @@ const { convertToPrice } = require('../libs/ethConvert');
 const etherscan = require('../libs/etherscan');
 
 // https://api.etherscan.io/api?module=account&action=balance&address=0xC6702D9937058b5186c633C8ebDA87996A4c283f&tag=latest&apikey=YourApiKeyToken
+// https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=0x2910543af39aba0cd09dbb2d50200b3e800a63d2&tag=latest&apikey=YourApiKeyToken
 const balance = (req, res, next) => {
 	const { address = "" } = req.query;
 	if (!address) {
@@ -11,17 +12,26 @@ const balance = (req, res, next) => {
 	}
 	const hash = address.match(/^0x/) ? address : `0x${address}`;
 
-	const option = {
+	const balanceOption = {
 		address,
 		module: 'account',
 		action: 'balance',
 		tag: 'latest',
+	};
+	const nonceOption = {
+		address,
+		module: 'proxy',
+		action: 'eth_getTransactionCount',
+		tag: 'latest'
 	}
-	Promise.all([etherscan(option), convertToPrice('cny')])
+	Promise.all([etherscan(balanceOption), convertToPrice('cny'), etherscan(nonceOption)])
 		.then(data => {
-			const tmp = parseInt(data[0].result) / 1e+18;
+			console.log(data);
+			const tBalance = data[0].result;
 			const price = data[1];
-	   	return responseSuccess(res, { balance: tmp, cny: tmp * price })
+			const nonce = !isNaN(parseInt(data[2].result)) ? parseInt(data[2].result) : 0;
+			const tmp = !isNaN(parseInt(tBalance)) ? parseInt(tBalance) / 1e+18 : 0;
+	   	return responseSuccess(res, { balance: tBalance, cny: tmp * price, nonce: nonce + 1 })
 		})
 		.catch(e => {
 			console.log('getBalance error', e);
