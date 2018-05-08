3aab94100afd8bbee6f85ff983f6421b48eb652e
old kidd <850477916@qq.com>
2018-05-08 22:58:02 +0800

    update

--- eth/routes/block.js
+++ eth/routes/block.js
@@ -10,21 +10,13 @@ const getBlockNumber = (req, res, next) => {
 	}
 	etherscan(option)
 		.then(data => {
-			const blockNumber = parseInt(data.result);
+			const blockNumber = isNaN(parseInt(data.result)) ? 0 : parseInt(data.result);
 	   	return responseSuccess(res, { blockNumber });
 		})
 		.catch(e => {
 			console.log('getBlockNumber error', e);
 	  	return responseError(res, 50000, e.message);
 		})
-	/*web3.eth.getGasPrice(function(err, result) {
-	  if (err){
-	    console.log('gasPrice error', err);
-	  	return responseError(res, 50000, err.message);
-	  }
-	   console.log('gasPrice success', result);
-	   return responseSuccess(res, { gasPrice: result });
-	});*/
 }
 
 module.exports = getBlockNumber;
