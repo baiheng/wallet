a9bc275c633fedd5245b31f8b572cd7ded84b627
bryant <465299904@qq.com>
2018-05-21 21:13:30 +0800

    去掉v1版本控制

--- bch/coin/urls.py
+++ bch/coin/urls.py
@@ -5,12 +5,12 @@ from coin.views.bch_view import BchView
 
 
 restful_urls = [
-    ("/v1/api/bch/broadcast", BchView, None, "post_action_broadcast", ["POST"]),
-    ("/v1/api/bch/fee", BchView, None, "get_action_fee", ["GET"]),
-    ("/v1/api/bch/balance", BchView, None, "get_action_balance", ["GET"]),
-    ("/v1/api/bch/utxo", BchView, None, "get_action_utxo", ["GET"]),
-    ("/v1/api/bch/transaction", BchView, None, "get_action_transaction", ["GET"]),
-    ("/v1/api/bch/transaction_detail", BchView, None, "get_action_transaction_detail", ["GET"]),
-    ("/v1/api/bch/block", BchView, None, "get_action_block", ["GET"]),
-    ("/v1/api/bch/decode_tx", BchView, None, "post_action_decode_tx", ["POST"]),
+    ("/api/bch/broadcast", BchView, None, "post_action_broadcast", ["POST"]),
+    ("/api/bch/fee", BchView, None, "get_action_fee", ["GET"]),
+    ("/api/bch/balance", BchView, None, "get_action_balance", ["GET"]),
+    ("/api/bch/utxo", BchView, None, "get_action_utxo", ["GET"]),
+    ("/api/bch/transaction", BchView, None, "get_action_transaction", ["GET"]),
+    ("/api/bch/transaction_detail", BchView, None, "get_action_transaction_detail", ["GET"]),
+    ("/api/bch/block", BchView, None, "get_action_block", ["GET"]),
+    ("/api/bch/decode_tx", BchView, None, "post_action_decode_tx", ["POST"]),
 ]
