4fc506118fb92e3346baf60482d3f1b0f40decf4
old kidd <850477916@qq.com>
2018-05-30 23:28:56 +0800

    tron 添加交易记录查询接口
    目前节点尚未支持

---
+++ tron/routes/transactionsToThis.js
@@ -0,0 +1,25 @@
+const { Account } = require('@tronprotocol/wallet-api/src/protocol/core/Tron_pb');
+const { responseError, responseSuccess } = require('../libs/response');
+
+const httpClient = require('../libs/TronClient').httpClient;
+const grpcClient = require('../libs/TronClient').grpcClient;
+const { getBase64AddressFromBase58 } = require('../libs/tool/address');
+const { convertToPrice } = require('../libs/convert');
+
+const Transaction = (req, res, next) => {
+	const { address = "" } = req.query;
+	if (!address) {
+		return responseError(res, 50001, 'address should not be empty');
+	}
+	const account = new Account(['', '', getBase64AddressFromBase58(address)]);
+	grpcClient.solidityApi.getTransactionsToThis(account)
+		.then(data => {
+			return responseSuccess(res, data.toObject());
+		})
+		.catch(e => {
+	    console.log('getTransactionsFromThis error', e);
+	  	return responseError(res, 50000, e);
+		});
+}
+
+module.exports = Transaction;
