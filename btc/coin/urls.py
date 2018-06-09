73cc3a3060a3e5e7e1d273cae45b8f49012cfddc
bryant <465299904@qq.com>
2018-06-09 13:58:27 +0800

    去除v1

--- btc/coin/urls.py
+++ btc/coin/urls.py
@@ -11,12 +11,12 @@ urls = [
 ]
 
 restful_urls = [
-    ("/v1/api/btc/broadcast", BtcView, btc_obj, "post_action_broadcast", ["POST"]),
-    ("/v1/api/btc/fee", BtcView, btc_obj, "get_action_fee", ["GET"]),
-    ("/v1/api/btc/balance", BtcView, btc_obj, "get_action_balance", ["GET"]),
-    ("/v1/api/btc/utxo", BtcView, btc_obj, "get_action_utxo", ["GET"]),
-    ("/v1/api/btc/transaction", BtcView, btc_obj, "get_action_transaction", ["GET"]),
-    ("/v1/api/btc/transaction_detail", BtcView, btc_obj, "get_action_transaction_detail", ["GET"]),
-    ("/v1/api/btc/block", BtcView, btc_obj, "get_action_block", ["GET"]),
-    ("/v1/api/btc/decode_tx", BtcView, btc_obj, "post_action_decode_tx", ["POST"]),
+    ("/api/btc/broadcast", BtcView, btc_obj, "post_action_broadcast", ["POST"]),
+    ("/api/btc/fee", BtcView, btc_obj, "get_action_fee", ["GET"]),
+    ("/api/btc/balance", BtcView, btc_obj, "get_action_balance", ["GET"]),
+    ("/api/btc/utxo", BtcView, btc_obj, "get_action_utxo", ["GET"]),
+    ("/api/btc/transaction", BtcView, btc_obj, "get_action_transaction", ["GET"]),
+    ("/api/btc/transaction_detail", BtcView, btc_obj, "get_action_transaction_detail", ["GET"]),
+    ("/api/btc/block", BtcView, btc_obj, "get_action_block", ["GET"]),
+    ("/api/btc/decode_tx", BtcView, btc_obj, "post_action_decode_tx", ["POST"]),
 ]
