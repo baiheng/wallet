4dbbc5ea7b304c2d57cf107fa3fa3965bca1c303
old kidd <850477916@qq.com>
2018-06-12 00:16:01 +0800

    tron 添加token相关操作

--- tron/routes/assetIssueList.js
+++ tron/routes/assetIssueList.js
@@ -1,18 +1,31 @@
 const { EmptyMessage } = require("@tronprotocol/wallet-api/src/protocol/api/api_pb");
+const apiClient = require('../libs/apiClient');
 
 const { responseError, responseSuccess } = require('../libs/response');
 const { httpClient, grpcClient, getGrpcClient } = require('../libs/TronClient');
 
-// 
+const PAGESIZE = 10;
+
+// https://api.tronscan.org/api/token?sort=-name&limit=25&start=25
 const assetIssueList = (req, res, next) => {
-	getGrpcClient().api.getAssetIssueList(new EmptyMessage())
-		.then(list => {
-	   return responseSuccess(res, list.toObject().assetissueList);
+	const { page } = req.query;
+	let pageNum = !isNaN(parseInt(page)) && parseInt(page) >= 0 ? parseInt(page) : 0;
+	apiClient(`token?sort=-name&limit=10&start=${pageNum * PAGESIZE}`)
+		.then(data => {
+			responseSuccess(res, data);
 		})
 		.catch(e => {
-	    console.log('getAssetIssueList error', e);
-	  	return responseError(res, 50000, err.message);
-		});
+			console.log('assetIssueList error', e);
+			responseError(res, 50000, e);
+		})
+	// getGrpcClient().api.getAssetIssueList(new EmptyMessage())
+	// 	.then(list => {
+	//    return responseSuccess(res, list.toObject().assetissueList);
+	// 	})
+	// 	.catch(e => {
+	//     console.log('getAssetIssueList error', e);
+	//   	return responseError(res, 50000, err.message);
+	// 	});
 }
 
 module.exports = assetIssueList;
---
+++ tron/routes/createAssetIssue.js
@@ -0,0 +1,41 @@
+const { AssetIssueContract } = require('@tronprotocol/wallet-api/src/protocol/core/Contract_pb');
+
+const { responseError, responseSuccess } = require('../libs/response');
+const getGrpcClient = require('../libs/TronClient').getGrpcClient;
+const { getBase64AddressFromBase58 } = require('../libs/tool/address');
+
+const transaction = async (req, res, next) => {
+    const { ownerAddress, name, totalSupply = 100000, trxNum, num, startTime, endTime, decayRatio, voteScore, description, url  } = req.body;
+    console.log(req.body)
+    if (!ownerAddress || !name) {
+        return responseError(res, 50001, 'params should not be empty');
+    }
+    const assetIssueContract = new AssetIssueContract([]);
+    assetIssueContract.setOwnerAddress(getBase64AddressFromBase58(ownerAddress));
+    assetIssueContract.setName(new Uint8Array(Buffer.from(name)));
+    assetIssueContract.setTotalSupply(totalSupply);
+    assetIssueContract.setTrxNum(trxNum);
+    assetIssueContract.setNum(num);
+    assetIssueContract.setStartTime(startTime);
+    assetIssueContract.setEndTime(endTime);
+    assetIssueContract.setDecayRatio(decayRatio);
+    assetIssueContract.setVoteScore(voteScore);
+    assetIssueContract.setDescription(new Uint8Array(Buffer.from(description)));
+    assetIssueContract.setUrl(new Uint8Array(Buffer.from(url)));
+    getGrpcClient().api.createAssetIssue(assetIssueContract)
+        .then(data => {
+            if (!data.getRawData())
+                throw data.toObject();
+            return responseSuccess(res, {
+            rawData: Buffer.from(data.getRawData().serializeBinary()).toString('base64'),
+        });
+
+        })
+        .catch(e => {
+            console.log('createTransaction error', e);
+            return responseError(res, 50000, e);
+        })
+}
+
+module.exports = transaction;
+
